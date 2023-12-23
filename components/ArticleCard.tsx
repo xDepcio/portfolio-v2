f664ecad495d527a10c02c484ff681d7f04d8ae7
xDepcio <olek.drwal@gmail.com>
2023-12-23 23:32:37 +0100

    added article card

---
+++ components/ArticleCard.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ArticleCard() {
+    return (
+        <Link href={'/'}>
+            <Image className="aspect-[16/9] rounded-lg mb-3" src="/example.jpg" alt="" width={500} height={0} />
+            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">Some article title</h3>
+            <p className="line-clamp-2 text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit voluptas libero, veniam qui consequatur minima magnam repellendus sunt dicta exercitationem, facilis cupiditate id nam omnis repudiandae optio. Non, perferendis consectetur.</p>
+            <div className="flex items-center justify-between mt-4">
+                <p className="text-muted-foreground text-sm">December 22, 2023</p>
+                <p className="text-yellow-700 text-sm bg-yellow-200 px-4 rounded-full">Not mine :(</p>
+            </div>
+        </Link>
+    )
+}
