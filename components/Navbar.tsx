cecb5d78a09fa2b4d0cb210eb1887c65dd19062d
xDepcio <olek.drwal@gmail.com>
2023-12-23 22:20:41 +0100

    added hover animations to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ import Cat from "./Cat";
 export default function Navbar() {
     return (
         <nav className="text-muted-foreground max-w-screen-xl mx-auto flex items-center justify-center">
-            <div className="flex gap-4 pt-8 font-semibold relative">
-                <Link href="/">Home</Link>
-                <Link href="/">Blog</Link>
+            <div className="flex gap-8 pt-8 font-semibold relative">
+                <Link href="/" className={`hover:text-slate-100 transition-all`}>Home</Link>
+                <Link href="/" className={`hover:text-slate-100 transition-all`}>Blog</Link>
                 <Cat />
             </div>
         </nav>
