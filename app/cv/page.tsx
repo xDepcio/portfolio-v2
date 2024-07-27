be7bfcd1296c8a40544a6835989c4c552db85ee3
xDepcio <olek.drwal@gmail.com>
2024-07-28 00:27:41 +0200

    updated skills

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -13,16 +13,16 @@ export default function CvPage() {
     return (
         <div className="">
             <div className="w-[1152px] m-auto relative">
-                <div className="absolute w-full h-[700px] -z-10 top-[600px] opacity-30" style={{ backgroundImage: `url(/hero-geo.svg)` }}>
+                <div className="absolute w-full h-[700px] -z-10 top-[460px] opacity-30" style={{ backgroundImage: `url(/hero-geo.svg)` }}>
                     <div className="innger-bg-geo "></div>
                 </div>
-                <section className="flex justify-center items-center mb-12 gap-8 mt-12">
-                    <div className="flex justify-center items-center relative">
+                <section className="flex justify-center items-center mb-20 gap-8 mt-12">
+                    {/* <div className="flex justify-center items-center relative">
                         <div className="w-[1200px] h-[600px] scale-[0.6] -left-[230px] -top-[60px] absolute" style={{ backgroundImage: `url(/test-2.svg)` }}>
 
                         </div>
                         <Image alt="profile-img" className="opacity-0" src="/bongo-cat.png" width={1300} height={980} />
-                    </div>
+                    </div> */}
                     <div>
                         <p className="text-lg text-muted-foreground font-medium">{`Hi! I'm`}</p>
                         <h1 className={cn(ProximaSoft.className, "text-6xl lg:text-7xl lg:whitespace-nowrap font-bold bg-gradient-to-br from-sky-500 to-sky-800  text-transparent bg-clip-text")}>Aleksander Drwal</h1>
@@ -45,7 +45,7 @@ export default function CvPage() {
                     </div>
                 </section>
                 <section className="lg:grid flex flex-col-reverse lg:grid-cols-2 lg:gap-24 gap-16">
-                    <div className="text-slate-50">
+                    <div className="text-slate-50 bg-slate-950">
                         <p className="text-amber-500 mb-6 text-xs text-center font-bold">Work experience</p>
                         <div className="border border-slate-900 rounded-lg p-4 flex flex-col gap-10">
                             <div className="grid grid-cols-[20%_80%]">
@@ -100,52 +100,68 @@ export default function CvPage() {
                         </div>
                     </div>
                 </section>
-                <section className="text-slate-100 mt-24">
-                    <h3 className="text-center font-medium text-muted-foreground">My skills</h3>
-                    <div className="flex gap-20 px-20 py-10 flex-wrap justify-center items-center">
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">React</p>
-                            <Image src={'/react.svg'} alt="skill image" width={50} height={50} />
+                <section className="text-slate-100 mt-20">
+                    <h3 className="text-center font-medium text-muted-foreground">Skills and technology</h3>
+                    <div className="flex gap-10 px-20 py-10 flex-wrap justify-center items-center">
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">TypeScript</p>
+                            <Image src={'/typescript.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">TypeScript</p>
-                            <Image src={'/typescript.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Python</p>
+                            <Image src={'/python.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">NodeJS</p>
-                            <Image src={'/node.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">C++</p>
+                            <Image src={'/c++.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">Express</p>
-                            <Image src={'/expressjs.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">NodeJS</p>
+                            <Image src={'/node.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">Sequelize</p>
-                            <Image src={'/sequelize.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">React</p>
+                            <Image src={'/react.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">Next.js</p>
-                            <Image src={'/nextjs.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Express</p>
+                            <Image src={'/expressjs.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">SQL</p>
-                            <Image src={'/sql.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Sequelize</p>
+                            <Image src={'/sequelize.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">Tailwind</p>
-                            <Image src={'/tailwind.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Next.js</p>
+                            <Image src={'/nextjs.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">Python</p>
-                            <Image src={'/python.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">SQL</p>
+                            <Image src={'/sql.svg'} alt="skill image" width={40} height={40} />
                         </div>
-                        <div className="bg-[#090F20] rounded-full w-32 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
-                            <p className="font-semibold text-muted-foreground text-sm">C++</p>
-                            <Image src={'/c++.svg'} alt="skill image" width={50} height={50} />
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Tailwind</p>
+                            <Image src={'/tailwind.svg'} alt="skill image" width={40} height={40} />
+                        </div>
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Docker</p>
+                            <Image src={'/docker.svg'} alt="skill image" width={40} height={40} />
+                        </div>
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Linux</p>
+                            <Image src={'/ubuntu.svg'} alt="skill image" width={40} height={40} />
+                        </div>
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Postgres</p>
+                            <Image src={'/postgres.svg'} alt="skill image" width={40} height={40} />
+                        </div>
+                        <div className="bg-[#090F20] rounded-full w-24 aspect-square flex justify-center flex-col items-center gap-2 shadow-md">
+                            <p className="font-medium text-muted-foreground text-xs">Nginx</p>
+                            <Image src={'/nginx.svg'} alt="skill image" width={40} height={40} />
                         </div>
                     </div>
                 </section>
-                <section className="text-slate-100 mt-24">
+                <section className="text-slate-100 mt-16">
                     <div className="flex items-center justify-start gap-2 text-muted-foreground font-medium">
                         <h3 className="">Recent projects</h3>
                         <HiChevronDown />
@@ -159,7 +175,6 @@ export default function CvPage() {
                                 <div>
                                     <h4 className="text-xl font-medium mt-4">Strapup - a template CLI</h4>
                                     <p className="text-sm text-muted-foreground">React, TypeScript, Next.js, NodeJS, TailwindCSS, CLI</p>
-                                    {/* <p className="mt-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit accusamus laborum minus sint id alias ipsam officia corporis. Ipsum exercitationem voluptatum inventore repellat vitae! Totam optio nostrum temporibus officia nisi?</p> */}
                                     <p className="mt-6">A project consisting of Strapup npm package - a CLI tool and a website for its doucmentation. It allows to quickly bootstrap any project by running custom scripts and pasting custom templates all from within interactive CLI.</p>
                                 </div>
                                 <div className="flex gap-4 items-center justify-start mt-10">
@@ -200,12 +215,6 @@ export default function CvPage() {
                                             <Code2 />
                                         </Link>
                                     </Button>
-                                    {/* <Button >
-                                        <Link href={'strapup.vercel.app'} className="flex items-center justify-center gap-2">
-                                            <p>View deployment</p>
-                                            <EyeIcon />
-                                        </Link>
-                                    </Button> */}
                                 </div>
                             </div>
                         </div>
