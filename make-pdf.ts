aad09ccaadc2499b79ad9ad9e17d1b6b29094f69
xDepcio <olek.drwal@gmail.com>
2024-07-28 01:26:05 +0200

    added script for printing cv to pdf

---
+++ make-pdf.ts
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+import * as fs from 'fs'
+global.btoa = (b) => Buffer.from(b).toString('base64')
+
+fetch('https://api.pdfshift.io/v3/convert/pdf', {
+    method: 'POST',
+    headers: {
+        Authorization: 'Basic ' + btoa(`api:${process.env.PDFSHIFT_API_KEY}`),
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+        source: 'https://www.adrwal.pl/cv',
+        landscape: false,
+        use_print: false,
+        sandbox: true,
+        format: `A4`,
+    })
+}).then(response => {
+    response.body!.pipe(fs.createWriteStream('cv.pdf'))
+})
