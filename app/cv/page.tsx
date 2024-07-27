4bf79c9c310d69f67f2c1cbd8efae88497844b28
xDepcio <olek.drwal@gmail.com>
2024-07-27 23:52:12 +0200

    added nested layout to host cv pdf scan page without navbar

---
+++ app/cv/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>CVhjson</h1>
+            <p>My CV</p>
+        </div >
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,27 @@
+import { cn } from '@/lib/utils'
+import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { cn } from '@/lib/utils'
-import Navbar from '@/components/Navbar'
-import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Aleksander Drwal',
-  description: `My personal portfolio showcasing my work and experience.`,
+    title: 'Aleksander Drwal',
+    description: `Personal portfolio showcasing my work and experience.`,
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={cn(inter.className, "overflow-x-hidden bg-slate-950")}>
-        <Navbar />
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={cn(inter.className, "overflow-x-hidden bg-slate-950")}>
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    )
 }
